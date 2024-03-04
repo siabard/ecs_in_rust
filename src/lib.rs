cce9a37d27f3914ea6eec0d9001ce9ac0ccbc5fa
Yeonho Jang <siabard@gmail.com>
2024-03-04 22:23:14 +0900

    add document for get_resource_mut

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,18 @@ impl World {
         self.resources.get_ref::<T>()
     }
 
+    /// Query for a resource and get a mutable reference to it. The type of the resource must be added in so that we can find it.
+    /// ```
+    /// use ecs_in_rust::World;
+    /// let mut world = World::new();
+    /// world.add_resource(10_u32);
+    /// {
+    ///   let resource = world.get_resource_mut::<u32>().unwrap();
+    ///   *resource += 1;    
+    /// }
+    /// let resource = world.get_resource::<u32>().unwrap();
+    /// assert_eq!(*resource, 11);
+    /// ```
     pub fn get_resource_mut<T: Any>(&mut self) -> Option<&mut T> {
         self.resources.get_mut::<T>()
     }
